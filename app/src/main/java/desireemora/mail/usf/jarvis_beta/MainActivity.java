96ae9170f1ba2304bfe73c25d51ab9997eba5550
Luis Massud <luique.massud@gmail.com>
2019-06-16 13:50:46 -0400

    Loading Screen -> Login
    updated .gitignore and loading screen goes to login now

--- app/src/main/java/desireemora/mail/usf/jarvis_beta/MainActivity.java
+++ app/src/main/java/desireemora/mail/usf/jarvis_beta/MainActivity.java
@@ -3,7 +3,10 @@ package desireemora.mail.usf.jarvis_beta;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.os.*;
+
+
+import desireemora.mail.usf.jarvis_beta.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run(){
+                // do something
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                // If you just use this that is not a valid context. Use ActivityName.this
+                startActivity(intent);
+            }
+        }, 2000);
     }
 
 
